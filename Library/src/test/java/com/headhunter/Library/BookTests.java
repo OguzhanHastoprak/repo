a41d0a46825a2a60448781ef5126f942fdf2bb23
ohanrak <160526539+ohanrak@users.noreply.github.com>
2024-03-19 20:27:22 +0300

    modifications for tests

--- Library/src/test/java/com/headhunter/Library/BookTests.java
+++ Library/src/test/java/com/headhunter/Library/BookTests.java
@@ -5,9 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -24,5 +29,43 @@ public class BookTests {
         this.mvc.perform(get("/api/v1/book/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1));
+    }
+
+    @Test
+    void shouldReturnAllBooksWhenListIsRequested() throws Exception {
+        this.mvc.perform(get("/api/v1/book"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4));
+    }
+
+    @Test
+    @DirtiesContext
+    @Transactional
+    @Rollback
+    @WithMockUser(username = "Admin", authorities = "ADMIN")
+    void shouldCreateANewBook() throws Exception {
+        String location = this.mvc.perform(post("/api/v1/book")
+                .contentType("application/json")
+                .content("""
+                        {
+                            "name": "Book F",
+                              "author": {
+                                "id": 1,
+                                "firstName": "Author",
+                                "lastName": "A"
+                              },
+                              "publisher": {
+                                "id": 1,
+                                "name": "Publisher A"
+                              }
+                        }
+                        """))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andReturn().getResponse().getHeader("Location");
+
+        this.mvc.perform(get(location))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Book F"));
     }
 }
